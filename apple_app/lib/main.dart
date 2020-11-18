6015b44891f527022f0d087455e8bf92b8c4195d
Smarajit Das <sdas9696969@gmail.com>
2020-11-19 00:49:57 +0530

    Initial

---
+++ apple_app/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final databaseReference = FirebaseFirestore.instance;
+  void createRecord(String loc) async {
+    await databaseReference
+        .collection("location")
+        .doc(DateTime.now().toString())
+        .set({'location': loc});
+  }
+
+  void getLoc() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    print(position);
+    createRecord(position.toString());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('App'),
+        ),
+        body: Container(
+          child: Center(
+            child: ElevatedButton(
+              child: Text('Press Me'),
+              onPressed: () {
+                getLoc();
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
